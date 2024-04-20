54181fee29859f4fa541e56da6a0ec38d9238361
Hero Protagonist <heroprotagonist32@gmail.com>
2024-04-20 12:48:05 -0400

    add utils

---
+++ src/utils/format-poems.ts
@@ -0,0 +1,10 @@
+export function formatPoems(poem: PoemDocument, chunk?: number) {
+    return (
+`${poem.title}
+
+${poem.text}
+
+        ${poem.book.author} — ${poem.book.title} (${poem.book.publishYear})
+`
+    )
+}
---
+++ src/utils/utils.ts
@@ -0,0 +1,5 @@
+import { formatPoems } from './format-poems';
+import { shuffleArray } from './shuffle-arr';
+
+export { formatPoems }
+export { shuffleArray }
