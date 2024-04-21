c145e532c03b4c6b1baff8166586b0a91dfbbf7c
Hero Protagonist <heroprotagonist32@gmail.com>
2024-04-21 00:38:24 -0400

    add non-esm worker for registry

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,41 @@
 import { webhookCallback } from 'grammy';
 import getBot from './bot';
 
-export default {
-
-  async fetch(req: Request , env: Env, c: ExecutionContext) {
-    let response = new Response('Bot initialization failed')
-
-    try {
-      const bot = await getBot(env)
-      response = await webhookCallback(bot, 'cloudflare-mod')(req)
-    }
-      catch(err){
-        console.log(err);
-    }
-    return response
-  },
-  async scheduled(e: ScheduledController, env: Env, c: ExecutionContext) {
+(async () => {
+  const bot = await getBot();
+  addEventListener("fetch", webhookCallback(bot, "cloudflare"))
+  addEventListener("scheduled", async (e: ScheduledEvent) => {
     switch (e.cron) {
       case "1 * * * *":
-        console.log('dispatch cron')
+        await bot.api.sendMessage(-4196751149, 'cronjob1')
         break;
     }
-    return
-  }
+  })
+})()
 
-} satisfies ExportedHandler<Env>
+// export default {
+
+//   async fetch(req: Request, env: Env, c: ExecutionContext) {
+//     let response = new Response('Bot initialization failed')
+
+//     try {
+//       const bot = await getBot(env)
+//       response = await webhookCallback(bot, 'cloudflare-mod')(req)
+//     }
+//     catch (err) {
+//       console.log(err);
+//     }
+
+//     return response
+//   },
+//   async scheduled(e: ScheduledController, env: Env, c: ExecutionContext) {
+//     switch (e.cron) {
+//       case "1 * * * *":
+//         const bot = await getBot(env)
+//         await bot.api.sendMessage(-4196751149, 'cronjob1')
+//       break;
+//     }
+//     return
+//   }env: Env
+
+// } satisfies ExportedHandler<Env>
